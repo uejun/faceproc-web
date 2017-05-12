1e08fd6b1b6229f47b77daa6138411087168ee0b
uejun <junya.ueda.iki@gmail.com>
2017-05-12 10:11:41 +0900

    Add use hash true.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,7 +42,8 @@ const appRoutes: Routes = [
     // },
     {
         path: 'reco-emotion',
-        component: FaceRecoEmotionComponent
+        component: FaceRecoEmotionComponent,
+        pathMatch: 'full'
     },
     // {
     //     path: 'make-emotion',
@@ -67,7 +68,7 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-      RouterModule.forRoot(appRoutes),
+      RouterModule.forRoot(appRoutes, { useHash: true }),
       BrowserAnimationsModule,
       MdButtonModule,
       MdGridListModule,
